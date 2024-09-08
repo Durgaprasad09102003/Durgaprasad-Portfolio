089776d89c8ed0eea4c74d2ecd2b6e221baedaec
PILLI DURGA PRASAD <100012584+Durgaprasad09102003@users.noreply.github.com>
2024-09-08 18:26:15 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
-import { Navigate, Route, Router, Routes } from 'react-router-dom';
-=======
->>>>>>> 02a51f6 (update)
 import Home from './pages/Home/Home';
 import './components/Component.css';
 import assets from './assets/assets';
@@ -12,21 +8,10 @@ import {  Navigate, Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <>
-<<<<<<< HEAD
-    <Router basename='/Durgaprasad-Portfolio'>
-      <Routes>
-        <Route path= '/Durgaprasad-Portfolio' element={<Navigate to='/home' />} />
-        <Route path= '/home' element={<Home />} />
-      </Routes>
-  </Router>
-
-      
-=======
       <Routes >
         <Route path='/' element={<Navigate to={"/home"} />} />
         <Route path="/home" element={<Home />} />
       </Routes>
->>>>>>> 02a51f6 (update)
     </>
   );
 }
