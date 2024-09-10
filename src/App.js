d0c51c913a96186304dba616a4d10a006bcaaff6
PILLI DURGA PRASAD <100012584+Durgaprasad09102003@users.noreply.github.com>
2024-09-10 15:46:24 +0530

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Home from './pages/Home/Home';
 import './components/Component.css';
 import assets from './assets/assets';
-import {  Navigate, Route, Routes } from 'react-router-dom';
+import {  Link, Navigate, Route, Routes } from 'react-router-dom';
 import AllProjects from './pages/AllProjects/AllProjects';
 import Header from './components/Header';
 import AllMedia from './pages/Media/AllMedia';
@@ -11,27 +11,19 @@ import AllMedia from './pages/Media/AllMedia';
 function App() {
   return (
     <>
-      <Routes>
-        <Route path='/' element={<Navigate to={"/home"} />} />
-        <Route path="/home" element={
-          <>
-            <Header /> 
-            <Home />
-          </>
-        } />
-        <Route path='/projects' element={
-          <>
-            <Header />
-            <AllProjects />
-          </>
-        } />
-        <Route path='/media' element={
-          <>
-            <Header />
-            <AllMedia />
-          </>
-        } />
-      </Routes>
+    <Header>
+      <Link to="/home">Home</Link>
+      <Link to="/projects">Projects</Link>
+      <Link to="/media">Media</Link>
+    </Header>
+
+
+    <Routes>
+      <Route path='/' element={<Navigate to={"/home"} />} />
+      <Route path="/home" element={<Home /> } />
+      <Route path='/projects' element={<AllProjects /> } />
+      <Route path='/media' element={<AllMedia /> } />
+    </Routes>
       
 
     </>
