70121df26bfbc9ad5839f0e64ab1ff601db50624
PILLI DURGA PRASAD <durgaprasadpilli02@gmail.com>
2025-01-09 18:51:37 +0530

    Initial commit

---
+++ src/pages/Media/AllMedia.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./AllMedia.css";
+
+function AllMedia() {
+  return (
+    <div className='Visume Visumemedia'>
+      <div className='template Visumemedia'>
+        <div className='videobar Visumemedia'>
+
+        </div>
+        <div className='naming Visumemedia'>
+          <hr />
+          <h2>DURGA PRASAD PILLI VISUME</h2>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AllMedia
